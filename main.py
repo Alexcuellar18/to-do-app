5166cd9bb526e0d6d51b9b0c8fbe757fd23b1e58
Alex Cuellar <alejandrocuellar18@gmail.com>
2019-09-26 00:14:31 -0400

    Submit button will store to DB and display on page.

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from pymysql import apilevel
+
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://to-do-app:Cuellara2013@localhost:8889/to-do-app'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-tasks = []
+class Task(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120))
+
+  def __init__(self,name):
+    self.name = name
+
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
+    
+    tasks = Task.query.all()
 
     return render_template('todos.html',title="To Do Application", tasks=tasks)
 
-
-app.run()
+if __name__ == '__main__':
+  app.run()
