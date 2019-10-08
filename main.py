b54cf493d3de1486d449fd77a0e9dcf4b507c8be
Alex Cuellar <alejandrocuellar18@gmail.com>
2019-10-08 16:43:58 -0400

    password encryption with hashing and salt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from pymysql import apilevel
+from hashutils import make_pw_hash,check_pw_hash
 
 
 
@@ -28,12 +29,12 @@ class User(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(120), unique=True)
-  password = db.Column(db.String(120))
+  pw_hash = db.Column(db.String(120))
   tasks = db.relationship('Task', backref='owner')
 
   def __init__(self,email,password):
     self.email = email
-    self.password = password
+    self.pw_hash = make_pw_hash(password)
 
 @app.before_request
 def require_login():
@@ -50,11 +51,9 @@ def login():
     email = request.form['email']
     password = request.form['password']
     user = User.query.filter_by(email=email).first()
-    if user and user.password == password:
+    if user and check_pw_hash(password,user.pw_hash):
       session['email'] = email
       flash('Logged In.')
-
-      #user logs in
       return redirect('/')
     else:
       flash('User password incorrect, or user does not exist','error')
